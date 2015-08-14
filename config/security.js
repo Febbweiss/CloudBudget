ffbc4f4980d6ebf6a5475b74f9dedff4f8134692
febbweiss <fabrice.ecaille@gmail.com>
2015-08-14 14:33:39 +0000

    Feature: add expiration to JWT

--- config/security.js
+++ config/security.js
@@ -1,7 +1,8 @@
 module.exports = {
     jwt : {
-        secretOrKey : 's3cr3t',
-        issuer      : undefined,    // accounts.examplesoft.com
-        audience    : undefined     // yoursite.net
+        secretOrKey         : 's3cr3t',
+        issuer              : undefined,    // accounts.examplesoft.com
+        audience            : undefined,    // yoursite.net
+        expiresInMinutes    : 60*8          // 8 hours
     }
 }
