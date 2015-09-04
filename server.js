9acd8af0358dde7259cf75404f84c605ea010df6
febbweiss <fabrice.ecaille@gmail.com>
2015-09-04 08:03:15 +0000

    Feature: allows CORS

--- server.js
+++ server.js
@@ -46,6 +46,13 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(methodOverride('X-HTTP-Method-Override'));
 app.use(express.static(__dirname + '/public'));
 app.use(errorHandler(server.errorHandlerOptions));
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+  res.header("Access-Control-Allow-Methods", "PUT, GET, POST, DELETE, OPTIONS");
+  next();
+});
+
 
 require('./app/routes')(app);
 
